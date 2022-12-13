fb773d31aa9b6b8d7084a60df10ea2217641581a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-13 18:47:04 +0000

    Adding tarfile member sanitization to extractall()

--- tools/exp_metrics_echarts.py
+++ tools/exp_metrics_echarts.py
@@ -18,7 +18,26 @@ if not os.path.exists(output_dir):
 
 # decompress the archive from experiment
 with tarfile.open(exp_archive_file, "r:gz") as tar_file:
-    tar_file.extractall(output_dir)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar_file, output_dir)
 
 # generate charts for latency
 latency_stat_file = os.path.join(output_dir, "exp_stat_latency.log")
